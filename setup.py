bd51865d7a7e520d594c7b8a83f8e0d7d9b85ee5
taxe10 <tanchavez@lbl.gov>
2023-10-03 15:38:16 -0700

    updating setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-setuptools.setup(
+setup(
     name="mlex_file_manager",                       # This is the name of the package
-    version="0.0.1",                                # The release version
+    version="0.0.2",                                # The release version
     author="Tanny Chavez E., Zhuowen (Kevin) Zhao", # Full name of the author
     author_email='tanchavez@lbl.gov, zzhao2@lbl.gov',
     description="",
     long_description=long_description,              # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(include=['file_manager', 'file_manager.*']),  # List of all python modules to be installed
+    packages=find_packages(include=['file_manager', 'file_manager.*', 'file_manager.*.*']),  # List of all python modules to be installed
     license_files = ('LICENSE.txt',),               # Information to filter the project on PyPi website
     classifiers=[
         'Environment :: Web Environment',
@@ -27,5 +27,6 @@ setuptools.setup(
         'Source': 'https://github.com/taxe10/mlex_file_manager.git'
     },
     python_requires='>=3.0',                        # Minimum version requirement of the package
+    package_dir={'':'file_manager'},     # Directory of the source code of the package
     install_requires=required                       # Install other dependencies if any
 )
