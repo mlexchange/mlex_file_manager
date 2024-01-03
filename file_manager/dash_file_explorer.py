1ef9053c234a2f8e7e20478b708673c3c477a56a
Tanny <tanchavez@lbl.gov>
2024-01-02 18:34:53 -0800

    redesigned frontend to use tabs instead of switch for file/tiled selection

--- file_manager/dash_file_explorer.py
+++ file_manager/dash_file_explorer.py
@@ -17,22 +17,92 @@ def create_file_explorer(max_file_size, default_tiled_uri=''):
         dbc.Card([
             dbc.CardBody(id={'base_id': 'file-manager', 'name': 'data-body'},
                         children=[
-                            ######################## UPLOADING DATA ################################
-                            dbc.Label('Upload a new file or a zipped folder:', className='mr-2'),
-                            html.Div([
-                                du.Upload(id={'base_id': 'file-manager', 'name': 'dash-uploader'},
-                                          max_file_size=max_file_size,
-                                          cancel_button=True,
-                                          pause_button=True,
-                                          default_style={'minHeight': 1,
-                                                         'lineHeight': 1}
-                                        )],
-                                style={'textAlign': 'center',
-                                       'width': '100%',
-                                       'padding': '5px',
-                                       'display': 'inline-block',
-                                       'margin-bottom': '10px',
-                                       'margin-right': '20px'},
+                            dcc.Tabs(
+                                id = {'base_id': 'file-manager', 'name': 'tabs'},
+                                value='file',
+                                children = [
+                                    dcc.Tab(
+                                        label='Filesystem',
+                                        value='file',
+                                        children=[
+                                            ######################## UPLOADING DATA ################################
+                                            html.P(),
+                                            dbc.Label(
+                                                'Upload a new file or a zipped folder:',
+                                                style = {
+                                                    'margin-right': '10px',
+                                                    'margin-bottom': '10px'
+                                                    }
+                                                ),
+                                            html.Div([
+                                                du.Upload(
+                                                    id={'base_id': 'file-manager', 'name': 'dash-uploader'},
+                                                    max_file_size=max_file_size,
+                                                    cancel_button=True,
+                                                    pause_button=True,
+                                                    default_style={
+                                                        'minHeight': 1,
+                                                        'lineHeight': 1}
+                                                    )],
+                                                style={
+                                                    'textAlign': 'center',
+                                                    'width': '100%',
+                                                    'padding': '5px',
+                                                    'display': 'inline-block',
+                                                    'margin-bottom': '10px',
+                                                    'margin-right': '20px'
+                                                    },
+                                            ),
+                                            ############################ FILE TABLE ################################
+                                            dbc.Row(
+                                                children=[
+                                                    dash_table.DataTable(
+                                                        id={'base_id': 'file-manager', 'name': 'files-table'},
+                                                        columns=[
+                                                            {'name': 'Type', 'id': 'type'},
+                                                            {'name': 'URI', 'id': 'uri'}
+                                                        ],
+                                                        data = [],
+                                                        page_size=5,
+                                                        hidden_columns = ['type'],
+                                                        row_selectable='single',
+                                                        style_cell={'padding': '0.5rem', 'textAlign': 'left'},
+                                                        fixed_rows={'headers': False},
+                                                        css=[{'selector': '.show-hide', 'rule': 'display: none'}],
+                                                        style_data_conditional=[
+                                                            {'if': {'filter_query': '{file_type} = dir'},
+                                                            'color': 'blue'},
+                                                            ],
+                                                        style_table={'overflowY': 'auto', 'margin-top': '10px'}
+                                                        ),
+                                                    ]
+                                                ),
+                                        ],
+                                    ),
+                                    dcc.Tab(
+                                        label='Tiled',
+                                        value='tiled',
+                                        children=[
+                                            ###################### TILED FOR DATA ACCESS ###########################
+                                            html.P(),
+                                            dbc.Label(
+                                                'Load data through Tiled:', 
+                                                style = {
+                                                    'margin-right': '10px',
+                                                    'margin-bottom': '10px'
+                                                    }
+                                                ),
+                                            dbc.InputGroup([
+                                                dbc.InputGroupText('URI'), 
+                                                dbc.Textarea(
+                                                    placeholder='tiled uri', 
+                                                    value=default_tiled_uri,
+                                                    style={'overflowY': 'scroll', 'height': '100px'},
+                                                    id={'base_id': 'file-manager', 'name': 'tiled-uri'})
+                                            ]),
+                                        ],
+                                    ),
+                                ]
                             ),
                             ################## BROWSE/IMPORT DATA FORMATS ##########################
                             dbc.Label('Choose file formats:', className='mr-2', 
@@ -88,79 +158,39 @@ def create_file_explorer(max_file_size, default_tiled_uri=''):
                                     width=5
                                 )
                             ], className="g-2", style={'display': 'None'}),   
-                            ###################### TILED FOR DATA ACCESS ###########################
-                            dbc.Label('Load data through Tiled:', 
-                                      style = {'margin-right': '10px',
-                                               'margin-bottom': '10px'}),   
-                            dbc.Row([
-                                dbc.Col(
-                                    dbc.InputGroup([
-                                        dbc.InputGroupText('URI'), 
-                                        dbc.Input(
-                                            placeholder='tiled uri', 
-                                            value=default_tiled_uri,
-                                            id={'base_id': 'file-manager', 'name': 'tiled-uri'})
-                                    ]),
-                                    width=10
-                                ),
-                                dbc.Col(
-                                    daq.BooleanSwitch(
-                                        id={'base_id': 'file-manager', 'name': 'tiled-switch'},
-                                        on=False,
-                                        color='green',
-                                        style={'width': '100%'}
-                                    ),
-                                    width=2
-                                ),
-                            ]),
-                            ############################ FILE TABLE ################################
-                            dbc.Row(
-                                children=[
-                                    dash_table.DataTable(
-                                        id={'base_id': 'file-manager', 'name': 'files-table'},
-                                        columns=[
-                                            {'name': 'Type', 'id': 'type'},
-                                            {'name': 'URI', 'id': 'uri'}
-                                        ],
-                                        data = [],
-                                        page_size=5,
-                                        hidden_columns = ['type'],
-                                        row_selectable='single',
-                                        style_cell={'padding': '0.5rem', 'textAlign': 'left'},
-                                        fixed_rows={'headers': False},
-                                        css=[{'selector': '.show-hide', 'rule': 'display: none'}],
-                                        style_data_conditional=[
-                                            {'if': {'filter_query': '{file_type} = dir'},
-                                            'color': 'blue'},
-                                            ],
-                                        style_table={'overflowY': 'auto', 'margin-top': '10px'}
-                                        ),
-                                    dcc.Store(id={'base_id': 'file-manager', 
-                                                  'name': 'docker-file-paths'}, 
-                                              data=[]),
-                                    dcc.Store(id={'base_id': 'file-manager', 
-                                                  'name': 'confirm-update-data'}, 
-                                              data=True),
-                                    dcc.Store(id={'base_id': 'file-manager', 
-                                                  'name': 'confirm-clear-data'}, 
-                                              data=False),
-                                    dcc.Store(id={'base_id': 'file-manager', 'name': 'upload-data'},
-                                              data=False),
-                                    dcc.Store(id={'base_id': 'file-manager', 'name': 'project-id'},
-                                              data=''),
-                                    ]
-                                ),
                             ########################## IMPORT BUTTON ###############################
                             dbc.Row(
-                                dbc.Button('Import',
-                                            id={'base_id': 'file-manager', 'name': 'import-dir'},
-                                            color='primary',
-                                            n_clicks=0,
-                                            style={'width': '40%'}
+                                dbc.Button(
+                                    'Import',
+                                    id={'base_id': 'file-manager', 'name': 'import-dir'},
+                                    color='primary',
+                                    n_clicks=0,
+                                    style={'width': '40%', 'margin-top': '10px'}
                                 ),
                                 justify='center'
                                 ),
                             ]),
-        ])
+                            ############################# CACHE ################################
+                            dcc.Store(
+                                id={'base_id': 'file-manager', 'name': 'docker-file-paths'}, 
+                                data=[]
+                                ),
+                            dcc.Store(
+                                id={'base_id': 'file-manager', 'name': 'confirm-update-data'}, 
+                                data=True
+                                ),
+                            dcc.Store(
+                                id={'base_id': 'file-manager', 'name': 'confirm-clear-data'}, 
+                                data=False
+                                ),
+                            dcc.Store(
+                                id={'base_id': 'file-manager', 'name': 'upload-data'},
+                                data=False
+                                ),
+                            dcc.Store(
+                                id={'base_id': 'file-manager', 'name': 'project-id'},
+                                data=''
+                                ),
+        ]),
     ])
     return file_explorer
