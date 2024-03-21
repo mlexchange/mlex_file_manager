461c72b0133c4072f24e27eb09b2925d71481109
Tanny <tanchavez@lbl.gov>
2024-03-20 20:01:03 -0700

    update example

--- fronty.py
+++ fronty.py
@@ -1,24 +1,32 @@
 import os
 import pathlib
+import time
 
 import dash
 import dash_bootstrap_components as dbc
+import dash_daq as daq
 import dash_uploader as du
 import diskcache
-from dash import Input, Output, dcc, html
+from dash import ALL, MATCH, ClientsideFunction, Input, Output, State, dcc, html
 from dash.long_callback import DiskcacheLongCallbackManager
+from flask_caching import Cache
 
 from file_manager.data_project import DataProject
 from file_manager.main import FileManager
+from plot_utils import draw_rows
 
-DATA_PATH = pathlib.Path.home() / "data"
+DATA_PATH = pathlib.Path.home() / "Documents" / "Data"
 UPLOAD_FOLDER_ROOT = DATA_PATH / "upload"
-SPLASH_URL = str(os.environ["SPLASH_URL"])
-TILED_KEY = str(os.environ["TILED_KEY"])
+SPLASH_URL = os.environ.get("SPLASH_URL", "http://splash:80/api/v0")
+TILED_KEY = os.environ.get("TILED_KEY", "")
+NUM_ROWS = 3
+NUM_COLS = 6
+TIMEOUT = 60
 
-#### SETUP DASH APP ####
+# SETUP DASH APP
 cache = diskcache.Cache("./cache")
 long_callback_manager = DiskcacheLongCallbackManager(cache)
+memoize_cache = Cache(config={"CACHE_TYPE": "filesystem", "CACHE_DIR": "./cache"})
 
 external_stylesheets = [
     dbc.themes.BOOTSTRAP,
@@ -30,8 +38,9 @@ app = dash.Dash(
     long_callback_manager=long_callback_manager,
 )
 app.title = "MLExchange Example"
+memoize_cache.init_app(app.server)
 
-#### SETUP FILE MANAGER ####
+# SETUP FILE MANAGER
 dash_file_explorer = FileManager(
     DATA_PATH,
     UPLOAD_FOLDER_ROOT,
@@ -42,7 +51,7 @@ dash_file_explorer = FileManager(
 dash_file_explorer.init_callbacks(app)
 du.configure_upload(app, UPLOAD_FOLDER_ROOT, use_upload_id=False)
 
-##### DEFINE LAYOUT ####
+# DEFINE LAYOUT
 app.layout = html.Div(
     [
         dbc.Container(
@@ -54,35 +63,80 @@ app.layout = html.Div(
                     [
                         dbc.CardHeader("Data display"),
                         dbc.CardBody(
-                            children=[
-                                dbc.Row(
-                                    [html.Img(id="img-output", style={"width": "20%"})],
-                                    justify="center",
-                                ),
-                                dbc.Row(
-                                    [
-                                        dbc.Label(
-                                            id="img-label", style={"height": "3rem"}
-                                        ),
-                                    ],
-                                ),
-                                dbc.Row(
-                                    [
-                                        dcc.Slider(
-                                            id="img-slider",
-                                            min=0,
-                                            step=1,
-                                            marks=None,
-                                            value=0,
-                                            tooltip={
-                                                "placement": "bottom",
-                                                "always_visible": True,
-                                            },
-                                        )
-                                    ]
-                                ),
-                            ]
+                            children=dcc.Tabs(
+                                id="viz-mode",
+                                value="gallery",
+                                children=[
+                                    dcc.Tab(
+                                        label="Gallery",
+                                        value="gallery",
+                                        children=draw_rows(NUM_ROWS, NUM_COLS)
+                                        + [
+                                            dbc.Row(
+                                                [
+                                                    dbc.Button(
+                                                        className="fa fa-chevron-left",
+                                                        id="prev-page",
+                                                        style={"width": "5%"},
+                                                    ),
+                                                    dbc.Button(
+                                                        className="fa fa-chevron-right",
+                                                        id="next-page",
+                                                        style={"width": "5%"},
+                                                    ),
+                                                ]
+                                            ),
+                                            dcc.Store(id="current-page", data=0),
+                                        ],
+                                    ),
+                                    dcc.Tab(
+                                        label="Single Image",
+                                        value="single",
+                                        children=[
+                                            dbc.Row(
+                                                [
+                                                    html.Img(
+                                                        id="img-output",
+                                                        style={"width": "20%"},
+                                                    )
+                                                ],
+                                                #  html.Canvas(id='image-canvas'),],
+                                                justify="center",
+                                            ),
+                                            dbc.Row(
+                                                [
+                                                    dbc.Label(
+                                                        id="img-label",
+                                                        style={"height": "3rem"},
+                                                    ),
+                                                ],
+                                            ),
+                                            dbc.Row(
+                                                [
+                                                    dcc.Slider(
+                                                        id="img-slider",
+                                                        min=0,
+                                                        step=1,
+                                                        marks=None,
+                                                        value=0,
+                                                        tooltip={
+                                                            "placement": "bottom",
+                                                            "always_visible": True,
+                                                        },
+                                                    )
+                                                ]
+                                            ),
+                                        ],
+                                    ),
+                                ],
+                            ),
                         ),
+                        daq.BooleanSwitch(
+                            id="log-transform", on=False, label="Log transform"
+                        ),
+                        dbc.Button("Download", id="download-button"),
+                        dcc.Store(id="download-data"),
+                        dcc.Store(id="unit-processed-data-store", data=None),
                     ]
                 ),
             ],
@@ -92,9 +146,126 @@ app.layout = html.Div(
 )
 
 
-#### CALLBACKS ####
-@app.callback(
+# CALLBACKS
+app.clientside_callback(
+    ClientsideFunction(namespace="clientside", function_name="transform_image"),
+    Output({"type": "thumbnail-src", "index": MATCH}, "src"),
+    Input("log-transform", "on"),
+    Input({"type": "thumbnail-src", "index": MATCH}, "src"),
+    State({"type": "processed-data-store", "index": MATCH}, "data"),
+    prevent_initial_call=True,
+)
+
+
+app.clientside_callback(
+    ClientsideFunction(namespace="clientside", function_name="transform_image"),
     Output("img-output", "src"),
+    Input("log-transform", "on"),
+    Input("img-output", "src"),
+    State("unit-processed-data-store", "data"),
+    prevent_initial_call=True,
+)
+
+
+@app.callback(
+    Output({"type": "thumbnail-src", "index": MATCH}, "src", allow_duplicate=True),
+    Input({"type": "processed-data-store", "index": MATCH}, "data"),
+    State({"type": "thumbnail-card", "index": MATCH}, "id"),
+    prevent_initial_call=True,
+)
+def update_thumbnail(processed_data, id):
+    # Update your thumbnails using the processed data from the dcc.Store
+    # Here, you can extract the specific part of the data relevant to this thumbnail
+    # based on its index or any other identifier included in the 'id' state
+    if processed_data is None:
+        return ""
+
+    # Example assuming processed_data is a list of dicts where each dict contains
+    # 'style', 'name', and 'src' keys for each thumbnail
+    return processed_data
+
+
+@app.callback(
+    Output({"type": "thumbnail-card", "index": ALL}, "style"),
+    Output({"type": "thumbnail-name", "index": ALL}, "children"),
+    Output({"type": "processed-data-store", "index": ALL}, "data"),
+    Input({"base_id": "file-manager", "name": "docker-file-paths"}, "data"),
+    Input("current-page", "data"),
+    prevent_initial_call=True,
+)
+@memoize_cache.memoize(timeout=TIMEOUT)
+def update_page(data_project_dict, current_page):
+    """
+    This callback updates the page
+    """
+    log = False
+    if data_project_dict:
+        data_project = DataProject.from_dict(data_project_dict)
+        if len(data_project.datasets) > 0:
+            n_images = NUM_ROWS * NUM_COLS
+            start = current_page * n_images
+            end = (current_page + 1) * n_images
+            if end > data_project.datasets[-1].cumulative_data_count:
+                end = data_project.datasets[-1].cumulative_data_count
+            start_time = time.time()
+            src_data, filenames = data_project.read_datasets(
+                list(range(start, end)),
+                log=log,
+            )
+            print(
+                f"Time to read page {current_page} {len(src_data)} images: {time.time() - start_time}",
+                flush=True,
+            )
+            return [{"display": "block"}] * len(filenames), filenames, src_data
+    return (
+        [{"display": "None"}] * NUM_ROWS * NUM_COLS,
+        [""] * NUM_ROWS * NUM_COLS,
+        [""] * NUM_ROWS * NUM_COLS,
+    )
+
+
+@app.callback(
+    Output("current-page", "data", allow_duplicate=True),
+    Input("prev-page", "n_clicks"),
+    State("current-page", "data"),
+    prevent_initial_call=True,
+)
+def update_page_prev(prev_page, current_page):
+    if current_page > 0:
+        return current_page - 1
+    return current_page
+
+
+@app.callback(
+    Output("current-page", "data", allow_duplicate=True),
+    Input("next-page", "n_clicks"),
+    State("current-page", "data"),
+    State({"base_id": "file-manager", "name": "total-num-data-points"}, "data"),
+    prevent_initial_call=True,
+)
+def update_page_next(next_page, current_page, total_num_data_points):
+    n_images = NUM_ROWS * NUM_COLS
+    n_pages = total_num_data_points // n_images + 1
+    if current_page < n_pages - 1:
+        return current_page + 1
+    return current_page
+
+
+@app.callback(
+    Output("download-data", "data"),
+    Input("download-button", "n_clicks"),
+    State({"base_id": "file-manager", "name": "docker-file-paths"}, "data"),
+)
+def download_data(n_clicks, data_project_dict):
+    if n_clicks:
+        data_project = DataProject.from_dict(data_project_dict)
+        data_project.tiled_to_local_project([0, 2, 4])
+        return "download"
+    return dash.no_update
+
+
+@app.callback(
+    Output("unit-processed-data-store", "data"),
     Output("img-slider", "max"),
     Output("img-slider", "value"),
     Output("img-label", "children"),
@@ -102,7 +273,7 @@ app.layout = html.Div(
     Input("img-slider", "value"),
     prevent_initial_call=True,
 )
-def refresh_image(file_paths, img_ind):
+def refresh_image(data_project_dict, img_ind):
     """
     This callback updates the image in the display
     Args:
@@ -114,20 +285,37 @@ def refresh_image(file_paths, img_ind):
         img-slider-value:   Current value of the slider
         label-output:       Output data set uri
     """
-    data_project = DataProject()
-    data_project.init_from_dict(file_paths)
-    if len(data_project.data) > 0:
-        slider_max = len(data_project.data) - 1
-        if img_ind > slider_max:
+    if data_project_dict:
+        data_project = DataProject.from_dict(data_project_dict)
+        if len(data_project.datasets) > 0:
+            slider_max = data_project.datasets[-1].cumulative_data_count - 1
+            if img_ind > slider_max:
+                img_ind = 0
+            image, uri = data_project.read_datasets([img_ind], log=False)
+            image = image[0]
+            uri = uri[0]
+        else:
+            image = dash.no_update
+            uri = dash.no_update
+            slider_max = 0
             img_ind = 0
-        image, uri = data_project.data[img_ind].read_data()
+        return image, slider_max, img_ind, uri
     else:
-        image = dash.no_update
-        uri = dash.no_update
-        slider_max = 0
-        img_ind = 0
-    return image, slider_max, img_ind, uri
+        return None, 0, 0, ""
+
+
+@app.callback(
+    Output("img-output", "src", allow_duplicate=True),
+    Input("unit-processed-data-store", "data"),
+    prevent_initial_call=True,
+)
+def update_image(image):
+    if image is None:
+        print("No image", flush=True)
+        return ""
+    print("There is an image", flush=True)
+    return image
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0")
+    app.run_server(debug=True)
