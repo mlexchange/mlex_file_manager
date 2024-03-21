8aa1644110fcc15e27936cdeb7a2cee42a408a8c
Tanny <tanchavez@lbl.gov>
2024-03-20 18:43:11 -0700

    update schema of base dataset

--- file_manager/dataset/dataset.py
+++ file_manager/dataset/dataset.py
@@ -1,11 +1,11 @@
 class Dataset:
-    def __init__(self, uri, type="file", **kwargs):
+    def __init__(self, uri, cumulative_data_count):
         """
         Base class for data set schema definition
         Args:
-            uri:            URI of the data set
-            data_type:      file or tiled
+            uri:                    Data set URI
+            cumulative_data_count:  Cumulative data count
         """
         self.uri = uri
-        self.type = type
+        self.cumulative_data_count = cumulative_data_count
         pass
